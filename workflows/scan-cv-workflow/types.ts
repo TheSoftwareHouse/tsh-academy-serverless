2ccd8300ce7e4bd637ca0d9b60edc280d7ac72c8
Jakub Cała <jakub.cala97@gmail.com>
2022-05-06 13:35:09 +0200

    Add starting point for workshops (#2)
    * feat: add starting point for workshops
    
    * refactor: rename copy lambda to example lambda

--- workflows/scan-cv-workflow/types.ts
+++ workflows/scan-cv-workflow/types.ts
@@ -1,17 +1,4 @@
 export interface ScanCvEvent {
   key: string;
   extension: string;
-  nodeExperience?: number;
-  cloudExperience?: number;
-  itExperience?: number;
-  isExcluded?: boolean;
-}
-
-export interface ScanCvScoreEvent {
-  key: string;
-  extension: string;
-  calculatedScore: number;
-  matchedFilesBucketName: string;
-  rejectedFilesBucketName: string;
-  copySource: string;
 }
