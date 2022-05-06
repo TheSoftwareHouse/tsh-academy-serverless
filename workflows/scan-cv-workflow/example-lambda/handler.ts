e65ff5635cb56e1e8ce06efcba302501ec5df496
Jakub Cała <jakub.cala@tsh.io>
2022-05-06 12:15:44 +0200

    refactor: rename copy lambda to example lambda

--- workflows/scan-cv-workflow/example-lambda/handler.ts
+++ workflows/scan-cv-workflow/example-lambda/handler.ts
@@ -1,12 +1,9 @@
 import { Context } from "aws-lambda";
 import { ScanCvEvent } from "../types";
-import { createConfig } from "./config";
 import { winstonLogger as logger } from "../../../shared/logger";
 
-const config = createConfig(process.env);
 
 export const handle = async (event: ScanCvEvent, _context: Context) => {
   logger.info("Handling event: %o", event);
-  logger.info(`Trying to move file ${event.key} from bucket ${config.inputBucketName}`);
   return event;
 };
