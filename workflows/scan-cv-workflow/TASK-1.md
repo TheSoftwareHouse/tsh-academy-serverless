a56e0baf032a16c7ae7f58470c6e543810bc2035
Jakub Cała <jakub.cala97@gmail.com>
2022-05-10 09:57:49 +0200

    Add starting point for task 2 (#3)
    * feat: add starting point for task 2
    
    * fix: lint issues

--- workflows/scan-cv-workflow/TASK-1.md
+++
@@ -1,7 +0,0 @@
-# TASK 1
-
-## Description
-Add to scan-file lambda a piece of code which will execute our workflow.
-
-### Useful links
-- [StepFunctions documentation](https://docs.aws.amazon.com/step-functions/latest/apireference/API_StartExecution.html)
--- workflows/scan-cv-workflow/example-lambda/handler.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from "aws-lambda";
-import { ScanCvEvent } from "../types";
-import { winstonLogger as logger } from "../../../shared/logger";
-
-
-export const handle = async (event: ScanCvEvent, _context: Context) => {
-  logger.info("Handling event: %o", event);
-  return event;
-};
