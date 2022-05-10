7b59c567a2ed3b516a5b5f56cc9a1a1839fe6dc5
Jakub Cała <jakub.cala@tsh.io>
2022-05-10 11:56:53 +0200

    feat: add task content for task 4

---
+++ workflows/scan-cv-workflow/calculate-score/handler.ts
@@ -0,0 +1,20 @@
+import { Context } from "aws-lambda";
+import { ScanCvEvent, ScanCvScoreEvent } from "../types";
+import { winstonLogger as logger } from "../../../shared/logger";
+import { createConfig } from "./config";
+
+const config = createConfig(process.env);
+
+export const handle = async (event: ScanCvEvent[], _context: Context) => {
+  logger.info(`Handling event: ${JSON.stringify(event)}`);
+
+  const singleEvent: Required<ScanCvEvent> = event.reduce((obj, currentValue) => {
+    return {
+      ...obj,
+      ...currentValue,
+    };
+  }, {} as any);
+
+  logger.info(`Score has been calculated for ${config.inputBucketName}/${singleEvent.key}`);
+  return event;
+};
