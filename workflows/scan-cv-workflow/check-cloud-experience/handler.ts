2418ef4b3ead15be7ee5e6b09947b49c734871d7
Piotr Jarolewski <p.jarolewski@atbank.nl>
2022-06-13 10:47:30 +0200

    feat: add missing test

--- workflows/scan-cv-workflow/check-cloud-experience/handler.ts
+++ workflows/scan-cv-workflow/check-cloud-experience/handler.ts
@@ -2,21 +2,24 @@ import { Context } from "aws-lambda";
 import { ScanCvEvent } from "../types";
 import { winstonLogger as logger } from "../../../shared/logger";
 import S3 from "aws-sdk/clients/s3";
-import { createConfig } from "./config";
+import { CheckCloudExperienceLambdaEnvironmentVariables, createConfig } from "./config";
 import { calculateScore } from "../utils";
 
 const KEYS_TO_SEARCH = ["aws", "google", "azure", "google cloud", "gcp"];
 
-const s3Client = new S3();
-const config = createConfig(process.env);
-
-export const handle = async (event: ScanCvEvent, _context: Context) => {
+export const checkCloudExperience = async (
+  event: ScanCvEvent,
+  config: CheckCloudExperienceLambdaEnvironmentVariables,
+  s3Client: S3,
+) => {
   logger.info("Handling event: %o", event);
 
-  const { Body: body } = await s3Client.getObject({
-    Bucket: config.extractedFilesBucketName,
-    Key: event.key,
-  }).promise();
+  const { Body: body } = await s3Client
+    .getObject({
+      Bucket: config.extractedFilesBucketName,
+      Key: event.key,
+    })
+    .promise();
 
   const cloudExperience = await calculateScore(body!.toString(), KEYS_TO_SEARCH);
 
@@ -25,4 +28,10 @@ export const handle = async (event: ScanCvEvent, _context: Context) => {
     ...event,
     cloudExperience,
   };
+};
+
+export const handle = async (event: ScanCvEvent, _context: Context) => {
+  const config = createConfig(process.env);
+  const s3Client = new S3();
+  return checkCloudExperience(event, config, s3Client);
 };
