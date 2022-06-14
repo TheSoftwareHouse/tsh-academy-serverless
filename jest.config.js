b35531f39008c7b5faeb093f29b2f8e2dfb9f186
Piotr Jarolewski <p.jarolewski@atbank.nl>
2022-06-14 10:23:08 +0200

    feat: add tests configuration

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  globals: {
+    "ts-jest": {
+      isolatedModules: true,
+    },
+  },
+};
