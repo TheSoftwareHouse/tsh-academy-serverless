4e0ef5096f18d60cb61c5bfa8090293755a45e11
Piotr Jarolewski <p.jarolewski@atbank.nl>
2022-06-15 10:14:50 +0200

    feat: add missing test

--- workflows/scan-cv-workflow/check-it-experience/handler.ts
+++ workflows/scan-cv-workflow/check-it-experience/handler.ts
@@ -2,21 +2,35 @@ import { Context } from "aws-lambda";
 import { ScanCvEvent } from "../types";
 import { winstonLogger as logger } from "../../../shared/logger";
 import S3 from "aws-sdk/clients/s3";
-import { createConfig } from "./config";
+import { CheckITExperienceLambdaEnvironmentVariables, createConfig } from "./config";
 import { calculateScore } from "../utils";
 
-const KEYS_TO_SEARCH = ["terraform", "kubernetes", "cypress", "mysql", "postgresql", "mongodb", "arangodb", "github", "docker", "ansible"];
+const KEYS_TO_SEARCH = [
+  "terraform",
+  "kubernetes",
+  "cypress",
+  "mysql",
+  "postgresql",
+  "mongodb",
+  "arangodb",
+  "github",
+  "docker",
+  "ansible",
+];
 
-const s3Client = new S3();
-const config = createConfig(process.env);
-
-export const handle = async (event: ScanCvEvent, _context: Context) => {
+export const checkItExperience = async (
+  event: ScanCvEvent,
+  config: CheckITExperienceLambdaEnvironmentVariables,
+  s3Client: S3,
+) => {
   logger.info("Handling event: %o", event);
 
-  const { Body: body } = await s3Client.getObject({
-    Bucket: config.extractedFilesBucketName,
-    Key: event.key,
-  }).promise();
+  const { Body: body } = await s3Client
+    .getObject({
+      Bucket: config.extractedFilesBucketName,
+      Key: event.key,
+    })
+    .promise();
 
   const itExperience = await calculateScore(body!.toString(), KEYS_TO_SEARCH);
 
@@ -25,4 +39,10 @@ export const handle = async (event: ScanCvEvent, _context: Context) => {
     ...event,
     itExperience,
   };
+};
+
+export const handle = async (event: ScanCvEvent, _context: Context) => {
+  const config = createConfig(process.env);
+  const s3Client = new S3();
+  return checkItExperience(event, config, s3Client);
 };
